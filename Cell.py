8e9dec65bc138771785c462d377f4cd07db7e2a5
Larlee <78880936+larleevee@users.noreply.github.com>
2022-09-09 18:57:45 +0100

    made very minor syntax changes

--- Cell.py
+++ Cell.py
@@ -27,13 +27,16 @@ class Cell(Constants):
         if self.visited == True:
             pygame.draw.rect(self.screen, pygame.Color(0,0,0), (self.x_len, self.y_len, self.tile, self.tile))
 
+       if self.is_start_cell or self.is_end_cell:
+            self.mark_cell(self)
+
         #draws in walls that the cell has left
         if self.walls["north"]:
             pygame.draw.line(self.screen, pygame.Color(230,230,250), (self.x_len, self.y_len), (self.x_len + self.tile, self.y_len), 2)
-        if self.walls["east"]:
-            pygame.draw.line(self.screen, pygame.Color(230,230,250), (self.x_len + self.tile, self.y_len), (self.x_len + self.tile, self.y_len + self.tile), 2)
         if self.walls["south"]:
             pygame.draw.line(self.screen, pygame.Color(230,230,250), (self.x_len + self.tile, self.y_len + self.tile), (self.x_len, self.y_len + self.tile), 2)
+        if self.walls["east"]:
+            pygame.draw.line(self.screen, pygame.Color(230,230,250), (self.x_len + self.tile, self.y_len), (self.x_len + self.tile, self.y_len + self.tile), 2)
         if self.walls["west"]:
             pygame.draw.line(self.screen, pygame.Color(230,230,250), (self.x_len, self.y_len + self.tile), (self.x_len, self.y_len), 2)
 
@@ -41,8 +44,8 @@ class Cell(Constants):
     def mark_cell(self):
         
         pygame.draw.rect(self.screen, pygame.Color(253,253,150), (self.x_len + 2, self.y_len + 2, self.tile - 2, self.tile - 2))
-
-
+        
+        
     def check_valid(self, grid, x_coord, y_coord): #makes sure a given cell is in the grid
 
         cols = self.get_cols()
@@ -53,26 +56,6 @@ class Cell(Constants):
             return False
         
         return grid[find_index(x_coord, y_coord)]
-    
-
-    def get_neighbours(self, grid):
-
-        neighbours = []
-        north = self.check_valid(grid, self.x_coord, self.y_coord - 1)
-        east = self.check_valid(grid, self.x_coord + 1, self.y_coord)
-        south = self.check_valid(grid, self.x_coord, self.y_coord + 1)
-        west = self.check_valid(grid, self.x_coord - 1, self.y_coord)
-
-        if north and not north.visited:
-            neighbours.append(north)
-        if east and not east.visited:
-            neighbours.append(east)
-        if south and not south.visited:
-            neighbours.append(south)
-        if west and not west.visited:
-            neighbours.append(west)
-
-        return neighbours
     
     
     def get_next(self, grid):
@@ -85,6 +68,28 @@ class Cell(Constants):
 
         if len(self.get_neighbours(grid)) != 0:
             return True
+        
+    def get_neighbours(self, grid):
+
+        neighbours = []
+        
+        north = self.check_valid(grid, self.x_coord, self.y_coord - 1)
+        if north and not north.visited:
+            neighbours.append(north)
+        
+        south = self.check_valid(grid, self.x_coord, self.y_coord + 1)
+        if south and not south.visited:
+            neighbours.append(south)
+        
+        east = self.check_valid(grid, self.x_coord + 1, self.y_coord)
+        if east and not east.visited:
+            neighbours.append(east)
+
+        west = self.check_valid(grid, self.x_coord - 1, self.y_coord)
+        if west and not west.visited:
+            neighbours.append(west)
+
+        return neighbours
     
 
     def remove_wall(self, current_cell, chosen_cell):
@@ -105,3 +110,8 @@ class Cell(Constants):
             current_cell.walls["south"] = False
             chosen_cell.walls["north"] = False
 
+    def mark_as_start(self):
+        self.is_start_cell = True
+
+    def mark_as_end(self):
+        self.is_end_cell = True
