33cae120f85733a16eb3000068c8eedd808ff476
Larlee <78880936+larleevee@users.noreply.github.com>
2022-10-09 08:44:46 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ def mainCode():
     grid.construct_grid()
     maze = grid.maze
     current_cell = grid.current_cell
-    player = Player()
+    player = Player(constants.get_tile())
 
     running = True
     constructing = True
@@ -28,6 +28,15 @@ def mainCode():
         for event in pygame.event.get(): #iterates through all user input/commands
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN: #if a key is pressed
+                if event.key == pygame.K_w:
+                    player.key_w()
+                if event.key == pygame.K_a:
+                    player.key_a()
+                if event.key == pygame.K_s:
+                    player.key_s()
+                if event.key == pygame.K_d:
+                    player.key_d()
 
         while constructing: #constructs the base maze
             screen.fill(constants.get_wall_colour())
@@ -52,7 +61,7 @@ def dfs(maze, clock, current_cell):
         chosen_cell = current_cell.get_next(maze)
         current_cell.remove_wall(current_cell, chosen_cell)
         pygame.display.flip()
-        clock.tick(80) #fps
+        clock.tick(120) #fps
         
         dfs(maze, clock, chosen_cell)
     
